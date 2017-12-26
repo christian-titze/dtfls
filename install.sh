055326c42e7cbc1519fb923d97f94d228bb99efd
Christian Titze <christian@Christians-MacBook-Pro.local>
2017-12-26 13:25:16 +0100

    Improve install script

--- install.sh
+++ install.sh
@@ -16,31 +16,48 @@ if [ "$(uname)" == "Linux" ]; then
     echo "Detected Linux distribution: $NAME $VERSION_ID"
     if [ "$NAME" == "Ubuntu" ]; then
       echo "Installing packages ..."
-      # Python dependencies for neovim.
-      sudo apt-get install python-dev python-pip python3-dev python3-pip
 
-      # Add stable neovim PPA.
-      sudo apt-get install software-properties-common
-      sudo add-apt-repository ppa:neovim-ppa/stable
-      sudo apt-get update
+      # Check if neovim PPA is already in sources.
+      NVIMPPA="neovim-ppa/stable"
+      grep -h "^deb.*$NVIMPPA" /etc/apt/sources.list.d/* > /dev/null 2>&1
+      if [ $? -ne 0 ]; then
+        # Add stable neovim PPA.
+        sudo apt-get install software-properties-common
+        sudo add-apt-repository -y ppa:$NVIMPPA
+        sudo apt-get update
+      else
+        echo "ppa:$NVIMPPA is already installed."
+      fi
+
+      # Python dependencies for neovim.
+      sudo apt-get install -y python-dev python-pip python3-dev python3-pip
 
       # Install packages.
-      sudo apt-get install wget curl cmake git zsh tmux vim neovim
+      sudo apt-get install -y wget curl gawk cmake git zsh tmux vim neovim
+
+      # Check if dotfiles dir already exists.
+      if [[ -d "${DOTFILES}" ]]; then
+        read -p "Directory ${DOTFILES} already exists, but is required to continue. Overwrite? [y/N] " -n 1 -r
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+          echo
+          rm -rf ${DOTFILES}
+        else
+          echo -e "\nInstallation aborted."
+          exit 1
+        fi
+      fi
 
       # Clone into dotfiles repo.
-      # TODO check if .dotfiles dir exists and is empty first
       git clone https://github.com/christian-titze/dtfls ${DOTFILES}
 
       # Symlink all configuration files.
-      ln -isv ${DOTFILES}/zsh/zshrc ${HOME}/.zshrc
-
-      # Install zplug the official way.
-      curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
-
-      # Run zsh and install plugins with zplug.
+      ln -isv ${DOTFILES}/zsh/.zshrc ${HOME}/.zshrc
 
       # Make zsh the default shell.
+      echo "Please enter your password to make zsh your default shell."
       chsh -s $(which zsh)
+
+      echo "zsh setup complete. Please log out or reboot your machine now."
 
     else
       echo "FAIL: Your Linux distribution is not supported. Installation aborted." >&2
