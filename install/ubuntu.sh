c02cf86b68856b540990bee267cbb493552954dd
Christian Titze <christian@Christians-MacBook-Pro.local>
2018-01-10 12:12:31 +0100

    add packages

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 echo "Installing packages ..."
 
 # Check if neovim PPA is already in sources.
@@ -19,7 +21,9 @@ sudo apt-get install -y python-dev python-pip python3-dev python3-pip
 
 # Install packages.
 sudo apt-get install -y wget curl gawk cmake git zsh vim neovim tmux xsel # Absolute minimum, do not remove any of these packages!
-sudo apt-get install -y build-essential tree # Nice-to-have packages.
+sudo apt-get install -y build-essential manpages-dev exuberant-ctags silversearcher-ag tree htop # Developer essentials.
+sudo apt-get install -y libc6-i386 lib32stdc++6 lib32ncurses5-dev gcc-multilib g++-multilib # Multiarch support.
+sudo apt-get install -y gdb gdb-multiarch strace ltrace binutils nasm nmap hexedit # Security essentials.
 
 # Make zsh the default shell.
 echo "Please enter your password to make zsh your default shell."
